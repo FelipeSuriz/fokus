a98f513b3ec34e175d3f5d88b39fbce817a6c7c7
Felipe Suriz <felipesuriz@hotmail.com>
2023-08-29 08:40:07 -0300

    Iniciando o temporizador setInterval

--- script.js
+++ script.js
@@ -4,20 +4,44 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const startPauseBt = document.querySelector('#start-pause')
+const botoes = document.querySelectorAll('.app__card-button')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+        musica.currentTime = 0
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function(contexto) {
+        contexto.classList.remove('active')        
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -43,4 +67,16 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    // iniciar()
+    tempoDecorridoEmSegundos -= 1
+    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`)
+}
+
+startPauseBt.addEventListener('click', contagemRegressiva)
+
+function iniciar() {
+    intervaloId = setInterval(contagemRegressiva, 1000)
 }
