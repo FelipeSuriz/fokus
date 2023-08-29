708de3e8e0a39523d9da792bcea4152130366dad
Felipe Suriz <felipesuriz@hotmail.com>
2023-08-29 10:08:25 -0300

    inserindo áudios no projeto

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const titulo = document.querySelector('.app__title')
 const startPauseBt = document.querySelector('#start-pause')
 const botoes = document.querySelectorAll('.app__card-button')
 
-let tempoDecorridoEmSegundos = 5
-let intervaloId = null
-
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 musica.loop = true
+const audioPlay = new Audio('/sons/play.wav')
+const audioPausa = new Audio('/sons/pause.mp3')
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 musicaFocoInput.addEventListener('change', () => {
     if (musica.paused) {
@@ -71,8 +74,9 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
-        zerar()
+        audioTempoFinalizado.play()
         alert('Tempo finalizado!')
+        zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
@@ -83,9 +87,11 @@ startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
     if (intervaloId) {
+        audioPausa.play()
         zerar()
         return
     }
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
